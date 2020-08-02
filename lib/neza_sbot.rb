765f058103e6a68eca00ed71cb30eb24136fe470
descholar-ceo <emmanuellamugi@gmail.com>
2020-08-02 20:58:23 +0200

    Add default

--- lib/neza_sbot.rb
+++ lib/neza_sbot.rb
@@ -1,8 +1,9 @@
 require 'slack-ruby-bot'
-require_relative '../config/environment'
 
-class NezaSbot < SlackRubyBot::Bot
-  command '/start' do |client, data, _match|
-    client.say(text: 'Hello world, welcome to the new world', channel: data.channel)
+class PongBot < SlackRubyBot::Bot
+  command 'ping' do |client, data, match|
+    client.say(text: 'pong', channel: data.channel)
   end
 end
+
+PongBot.run
