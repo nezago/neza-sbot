85ee05debeb44135f2deb5900562b9d472b9acc7
descholar-ceo <emmanuellamugi@gmail.com>
2020-08-03 14:01:54 +0200

    Add pr description

--- modules/messages.rb
+++ modules/messages.rb
@@ -14,6 +14,7 @@ module Messages
     1. GitHub: https://www.github.com/nezago
     2. Slack: https://nezago.slack.com
     3. Medium: https://medium.com/@emmamugira
+    4. Email: neza.nezago@gmail.com
     
     \t\t\t\t\tGUIDELINES AND BESTPRACTICES AT NEZAGO
     \t\t\t\t\t======================================
@@ -31,5 +32,37 @@ module Messages
     - Add signup endpoint
     
     [Finishes#1111111]
+    "
+
+    PR_SNIPPET="
+
+    The following is a template of a pull request description:
+    =========================================================
+   
+    ## What does it do?
+    >Explain what does this PR do in no more than 2 lines
+   
+   ## Tasks to be completed in this PR
+   > List all tasks that you have completed and remaining in this PR
+   
+   ## How is it tested locally?
+   
+      ### Pre-Requisites
+   > Explain what should I have installed on my computer, to be able to test this PR
+   
+      ### Steps
+   > Elaborate all needed steps to test this PR on a local computer
+   
+   ## Pivotal tracker story (ies)
+   > Pivotal tracker story related to this PR
+   
+   ## Screenshot (s)
+   > Add the UI screenshots or endpoind testing result here
+   
+   ### Question (s)
+   > Any question you got while working on this PR
+
+
+   Or get it from this link: https://raw.githubusercontent.com/nezago/nezago-guidelines/development/nezago-templates/nezago-pull-request-description-template.md
     "
 end
